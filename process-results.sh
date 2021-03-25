07ca4a0b8c880f394d48cee62d23b65876e55e93
Warren Bailey <warren@warrenbailey.net>
2021-03-25 16:01:06 +0000

    initial check in

---
+++ process-results.sh
@@ -0,0 +1,20 @@
+
+# Get benchmark outputs
+OUTPUT_BUCKET="$OUTPUT_BUCKET" \
+OUTPUT_DIR="outputs" \
+NUMBER_OF_DAYS="$NUMBER_OF_DAYS" \
+pipenv run python -m scripts.get_benchmark_results
+
+# Create performance graph
+OUTPUT_DIR="outputs/${OUTPUT_DIR}" \
+NUMBER_OF_DAYS="$NUMBER_OF_DAYS" \
+pipenv run python -m scripts.visualise_results
+
+# Date to get summary for
+RUNTIME_DATE_STRING="$(date +'%Y-%m-%d')"
+SUMMARY=$(OUTPUT_DIR="outputs/${OUTPUT_DIR}" \
+OUTPUT_DATE="$RUNTIME_DATE_STRING" \
+pipenv run python -m scripts.get_summary)
+
+
+pipenv run python -m scripts.store_benchmark_outputs
---
+++ scripts/store_benchmark_outputs.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime
+
+from scripts.google_cloud_storage import GoogleCloudStorage
+
+'''
+This script allows the upload of the benchmark outputs to a GCS Bucket.
+If running locally, you must specify valid Application Default Credentials (ADC):
+https://cloud.google.com/docs/authentication/production
+'''
+if __name__ == '__main__':
+    gcs_bucket_name = os.environ['GCS_OUTPUT_BUCKET']
+
+    if not gcs_bucket_name:
+        raise ValueError(
+            'Value for `GCS_OUTPUT_BUCKET` environment variable not valid.'
+        )
+
+    host = os.getenv('HOST')
+    output_directory = os.getenv('OUTPUT_DIRECTORY')
+    filename_prefix = os.getenv('OUTPUT_FILENAME_PREFIX')
+
+    gcs = GoogleCloudStorage(bucket_name=gcs_bucket_name)
+    gcs.upload_files(
+        output_files=(
+            'performance_graph.png'
+        ),
+        directory=output_directory,
+        output_filename_prefix=filename_prefix,
+        timestamp=int(datetime.utcnow().timestamp()),
+    )
